2459cf80cc951fe061781dbe5483e57f35d7ab1d
zhoudeguang <zhoudeguang@future.com.cn>
2018-11-26 17:20:26 +0800

    1. +Listing 3.5 A fleshed-out class definition for a thread-safe stack. 2. + common_def.h: define some const var for thread testing.
    1. +Listing 3.5 A fleshed-out class definition for a thread-safe stack.
    2. + common_def.h: define some const var for thread testing.

--- cpp_concurrency_in_action/cpp_concurrency_in_action.cpp
+++ cpp_concurrency_in_action/cpp_concurrency_in_action.cpp
@@ -44,9 +44,10 @@ int main() {
 
 #if 0//chapter3
     thread_sharing_data::foo();
+    thread_sharing_data::stack_test();
 #endif
 
-    thread_sharing_data::stack_test();
+    thread_sharing_data::thread_safe_stack_test();
 
     return 0;
 }
