ff17350e2716fa633d6911aaca4aee5697589130
deguangchow <deguangchow@qq.com>
2018-12-13 23:26:12 +0800

    +6.3 Designing more complex lock-based data structures; +6.3.1 Writing a thread-safe lookup table using locks; +Listing 6.11 A thread-safe lookup table;

--- cpp_concurrency_in_action/lock_based_concurrent_data_structures.h
+++ cpp_concurrency_in_action/lock_based_concurrent_data_structures.h
@@ -472,6 +472,101 @@ void threadsafe_waiting_queue_read();
 void threadsafe_waiting_queue_loop();
 void threadsafe_waiting_queue_test();
 
+//6.3 Designing more complex lock-based data structures
+//6.3.1 Writing a thread-safe lookup table using locks
+//Listing 6.11 A thread-safe lookup table
+#define USE_BOOST_SHARED_LOCK 0
+template<typename Key, template Value, typename Hash = std::hash<Key>>
+class threadsafe_lookup_table {
+private:
+    class bucket_type {
+    private:
+        typedef std::pair<Key, Value> bucket_value;
+        typedef std::list<bucket_value> bucket_data;
+        typedef typename bucket_data::iterator bucket_iterator;
+        bucket_data data;
+#if USE_BOOST_SHARED_LOCK
+        mutable boost::shared_mutex mutex;
+#else
+        mutable std::mutex mutex;
+#endif
+        bucket_iterator find_entry_for(Key const& key) const {
+            TICK();
+            return std::find_if(data.begin(), data.end(), [&](bucket_value const& item) {return item.first == key; });
+        }
+
+    public:
+        Value value_for(Key const& key, Value const& default_value)const {
+            TICK();
+#if USE_BOOST_SHARED_LOCK
+            std::shared_lock<boost::shared_mutex> lock(mutex);
+#else
+            std::unique_lock<std::mutex> lock(mutex);
+#endif
+            bucket_iterator const found_entry = find_entry_for(key);
+            return (found_entry == data.end()) ? default_value : found_entry->second;
+        }
+        void add_or_update_mapping(Key const& key, Value const& value) {
+            TICK();
+#if USE_BOOST_SHARED_LOCK
+            std::unique_lock<boost::shared_mutex> lock(mutex);
+#else
+            std::unique_lock<std::mutex> lock(mutex);
+#endif
+            bucket_iterator const found_entry = find_entry_for(key);
+            if (found_entry == data.end()) {
+                data.push_back(bucket_value(key, value));
+            } else {
+                found_entry->second = value;
+            }
+        }
+        void remove_mapping(Key const& key) {
+            TICK();
+#if USE_BOOST_SHARED_LOCK
+            std::unique_lock<boost::shared_mutex> lock(mutex);
+#else
+            std::unique_lock<std::mutex> lock(mutex);
+#endif
+            bucket_iterator const& found_entry = find_entry_for(key);
+            if (found_entry != data.end()) {
+                data.erase(found_entry);
+            }
+        }
+    };
+    std::vector<std::unique_ptr<bucket_type>> buckets;
+    Hash hasher;
+
+    bucket_type& get_bucket(Key const& key) const {
+        TICK();
+        std::size_t const bucket_index = hasher(key) % buckets.size();
+        return *buckets[bucket_index];
+    }
+
+public:
+    typedef Key key_type;
+    typedef Value mapped_type;
+    typedef Hash hash_type;
+
+    explicit threadsafe_lookup_table(unsigned num_buckets = 19, Hash const& hasher_ = Has()) :
+        buckets(num_buckets), hasher(hasher_) {
+        TICK();
+        for (unsigned i = 0; i < num_buckets; ++i) {
+            buckets[i].reset(new bucket_type);
+        }
+    }
+
+    threadsafe_lookup_table(threadsafe_lookup_table const& other) = delete;
+    threadsafe_lookup_table& operator=(threadsafe_lookup_table const& other) = delete;
+
+    Value value_for(Key const& key, Value const& default_value = Value()) const {
+        TICK();
+        return get_bucket(key).value_for(key, default_value);
+    }
+    void add_or_update_mapping(Key const& key, Value const& value) {
+        TICK();
+    }
+};
+
 
 }//namespace lock_based_conc_data
 
