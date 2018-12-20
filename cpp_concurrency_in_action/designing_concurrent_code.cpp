e1b1dcca4c01312d532a2927a69f072629bfd6bb
zhoudeguang <zhoudeguang@future.com.cn>
2018-12-20 12:43:37 +0800

    +chapter8: Designing concurrent code

---
+++ cpp_concurrency_in_action/designing_concurrent_code.cpp
@@ -0,0 +1,15 @@
+///    Copyright (C) 2018 DG.C, DGCHOW, deguangchow
+///        deguangchow@qq.com
+///
+///    \brief    chapter8: Designing concurrent code
+///
+///    \author   deguangchow
+///    \version  1.0
+///    \2018/12/20
+#include "stdafx.h"
+#include "designing_concurrent_code.h"
+
+namespace design_conc_code {
+
+}//namespace design_conc_code
+
---
+++ cpp_concurrency_in_action/designing_concurrent_code.h
@@ -0,0 +1,18 @@
+///    Copyright (C) 2018 DG.C, DGCHOW, deguangchow
+///        deguangchow@qq.com
+///
+///    \brief    chapter8: Designing concurrent code
+///
+///    \author   deguangchow
+///    \version  1.0
+///    \2018/12/20
+#pragma once
+#ifndef DESIGNING_CONCURRENT_CODE_H
+#define DESIGNING_CONCURRENT_CODE_H
+
+namespace design_conc_code {
+
+}//namespace design_conc_code
+
+#endif  //DESIGNING_CONCURRENT_CODE_H
+
