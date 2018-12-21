565f81e4d98d72d10a9de82ec679278c480dbd17
deguangchow <deguangchow@qq.com>
2018-12-21 23:33:29 +0800

    +Listing 8.8 A parallel version of std::for_each using std::async

--- cpp_concurrency_in_action/cpp_concurrency_in_action.cpp
+++ cpp_concurrency_in_action/cpp_concurrency_in_action.cpp
@@ -134,9 +134,10 @@ int main() {
     design_conc_code::parallel_accumulate_test();
     design_conc_code::parallel_accumulate_join_test();
     design_conc_code::parallel_accumulate_async_test();
+    design_conc_code::parallel_for_each_test();
 #endif
 
-    design_conc_code::parallel_for_each_test();
+    design_conc_code::parallel_for_each_async_test();
 
     return 0;
 }
